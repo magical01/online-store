caa9875db690652680e3ce0cfeda9a7ee26491e2
magical01 <denis.verzakov.015@gmail.com>
2022-06-01 17:18:51 +0500

    feat: add adaptive for banner section

--- js/main.js
+++ js/main.js
@@ -2,7 +2,15 @@ $(function(){
   $('.banner-section__slider').slick({
     dots: true,
     prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
-    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>'
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
+    responsive: [ 
+    {
+      breakpoint: 975,
+      settings: {
+        arrows: false
+      }
+    }
+  ] 
   });
 
   $('.tab').on('click', function(event) {
